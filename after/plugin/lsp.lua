ed3525db7990616640775cc5f97d625ec63279c6
Adroaldo de Andrade <adroaldof@gmail.com>
2022-12-26 12:44:27 -0300

    Configure Mason borders

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,9 @@
+require('mason.settings').set({
+  ui = {
+    border = 'rounded'
+  }
+})
+
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
@@ -41,7 +47,13 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-	suggest_lsp_servers = false,
+  suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = true,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = 'local',
 	sign_icons = {
 		error = '✘',
 		warn = '▲',
