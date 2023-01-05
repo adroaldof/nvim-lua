71264ad835fc5e897a07116fe9c1742b7d207c09
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-05 12:16:30 -0300

    Add initial comment lua setup

---
+++ after/plugin/comment.lua
@@ -0,0 +1,7 @@
+local comment_status, comment = pcall(require, "Comment")
+if not comment_status then
+    print("Please, ensure install of `windwp/nvim-comment.nvim` plugin")
+    return
+end
+
+comment.setup()
