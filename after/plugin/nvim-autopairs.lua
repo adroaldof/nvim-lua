10530fdef817e5609ad78ee96e05aa22c1902a5c
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-05 12:19:07 -0300

    Update autopairs setup

--- after/plugin/nvim-autopairs.lua
+++ after/plugin/nvim-autopairs.lua
@@ -1,3 +1,9 @@
-require("nvim-autopairs").setup({
-	disable_filetype = { "TelescopePrompt", "vim" },
+local autopairs_status, autopairs = pcall(require, "nvim-autopairs")
+if not autopairs_status then
+    print("Please, ensure install of `windwp/nvim-autopairs.nvim` plugin")
+    return
+end
+
+autopairs.setup({
+    disable_filetype = { "TelescopePrompt", "vim" },
 })
