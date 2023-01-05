0fce1d768b9905193d03e4817b0f69a5413147c3
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-05 12:27:53 -0300

    Update format setup

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,67 +1,72 @@
+local formatter_status, formatter = pcall(require, "formatter")
+if not formatter_status then
+    print("Please, ensure install of `mhartington/formatter.nvim` plugin")
+    return
+end
+
 local util = require("formatter.util")
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup({
-	-- Enable or disable logging
-	logging = true,
-	-- Set the log level
-	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
-	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
-		lua = {
-			-- "formatter.filetypes.lua" defines default configurations for the
-			-- "lua" filetype
-			require("formatter.filetypes.lua").stylua,
+local configurePrettier = function()
+    return {
+        exe = "prettier",
+        args = { "--stdin-filepath", vim.fn.shellescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
+        stdin = true,
+    }
+end
 
-			-- You can also define your own configuration
-			function()
-				-- Supports conditional formatting
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
+local languageFormatters = {
+    lua = {
+        require("formatter.filetypes.lua").stylua,
+        function()
+            if util.get_current_buffer_file_name() == "special.lua" then
+                return nil
+            end
 
-				-- Full specification of configurations is down below and in Vim help
-				-- files
-				return {
-					exe = "stylua",
-					args = {
-						"--search-parent-directories",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					},
-					stdin = true,
-				}
-			end,
-		},
+            return {
+                exe = "stylua",
+                args = {
+                    "--search-parent-directories",
+                    "--stdin-filepath",
+                    util.escape_path(util.get_current_buffer_file_path()),
+                    "--",
+                    "-",
+                },
+                stdin = true,
+            }
+        end,
+    },
 
-		javascript = {
-			require("formatter.filetypes.javascript").prettier,
-		},
+    ["*"] = {
+        require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+}
 
-		javascriptreact = {
-			require("formatter.filetypes.javascriptreact").prettier,
-		},
+local languages = {
+    "css",
+    "html",
+    "java",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "markdown",
+    "markdown.mdx",
+    "scss",
+    "svelte",
+    "svg",
+    "typescript",
+    "typescriptreact",
+    "xml",
+    "yaml",
+}
 
-		typescript = {
-			require("formatter.filetypes.typescript").prettier,
-		},
+for _, language in ipairs(languages) do
+    languageFormatters[language] = { configurePrettier }
+end
 
-		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettier,
-		},
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
-		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
-	},
+formatter.setup({
+    logging = true,
+    log_level = vim.log.levels.WARN,
+    filetype = languageFormatters,
 })
 
 vim.cmd([[
