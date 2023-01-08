c7e35aba4fb3a5d6f2df9742dc55138cdb7bc3c3
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-08 13:43:03 -0300

    Change git signs

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,7 +1,12 @@
 local git_status, git = pcall(require, "gitsigns")
 if not git_status then
-    print("Please, ensure install of `lewis6991/gitsigns.nvim` plugin")
     return
 end
 
-git.setup()
+git.setup({
+    signs = {
+        delete = { hl = "GitSignsDelete", text = "►", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "◥", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "⫸", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    },
+})
