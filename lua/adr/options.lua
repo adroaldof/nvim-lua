c6a19ec76784bc47ea4ad525508099d8e6ae55f2
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-14 18:04:08 -0300

    Update vim options

--- lua/adr/options.lua
+++ lua/adr/options.lua
@@ -5,13 +5,15 @@
 -- File Context
 -------------------------------------------------------------------------------
 vim.opt.number = true -- Line numbers are good
+vim.opt.writebackup = false
+vim.opt.backup = false
 vim.opt.relativenumber = true -- Set relative numbers to vim
 vim.opt.colorcolumn = "80,120" -- Show column for max line length
 vim.opt.signcolumn = "yes" -- Show sign column
 vim.opt.scrolloff = 4 -- Min number lines of context
 vim.opt.fileencoding = "utf-8" -- The encoding written to a file
--- vim.opt.spell = true
--- vim.opt.spelllang = "en"
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -------------------------------------------------------------------------------
 -- Turn off swap files
@@ -21,6 +23,7 @@ vim.opt.backup = false -- Disable backup file
 vim.opt.wb = false -- Disable write buffer
 vim.opt.writebackup = false -- Disable write backup file
 vim.opt.undofile = true -- Enable persistent undo
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undodir"
 
 -------------------------------------------------------------------------------
 -- Split Windows
