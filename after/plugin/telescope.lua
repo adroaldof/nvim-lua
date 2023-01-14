30bd32fd5fb26431fb573b0be5aa0ef59bd07a3e
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-14 18:49:20 -0300

    Add buffers to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,4 +2,5 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>p", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep theme=ivy<cr>")
