a292424908752c2f90278632096e7b430754804a
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-14 18:06:10 -0300

    Remove duplicate keymap <C-k>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,5 +2,4 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>p", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<C-r>", builtin.grep_string, {})
 vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep theme=ivy<cr>")
