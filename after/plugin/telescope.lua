1585e6cd08a115464f1ed17cc286f1c09002df2e
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-14 19:11:16 -0300

    Add protect call on telescope configs

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,8 @@
-local builtin = require("telescope.builtin")
+local is_telescope_ok, builtin = pcall(require, "telescope.builtin")
+if not is_telescope_ok then
+	print("Please, ensure install of `nvim-telescope/telescope.nvim` plugin")
+	return
+end
 
 vim.keymap.set("n", "<leader>p", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
