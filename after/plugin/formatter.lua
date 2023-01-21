3f7db5a66411ea836a05b25f84c829d8333b3569
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-20 21:30:52 -0300

    Fix null-ls configurations

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,29 +1,42 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	print("Please, ensure install of `jose-elias-alvarez/null-ls.nvim` plugin")
-	return
+    print("Please, ensure install of `jose-elias-alvarez/null-ls.nvim` plugin")
+    return
 end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local completion = null_ls.builtins.completion
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- TS
-		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
-		diagnostics.eslint,
-		-- Python
-		formatting.black.with({ extra_args = { "--fast" } }),
-		diagnostics.flake8,
-		-- Lua
-		formatting.stylua,
-	},
+    debug = false,
+    sources = {
+        -- TS
+        formatting.prettier,
+        diagnostics.eslint,
+        -- Python
+        formatting.black.with({ extra_args = { "--fast" } }),
+        diagnostics.flake8,
+        -- Lua
+        formatting.stylua,
+        -- spell
+        completion.luasnip,
+        completion.spell,
+        -- Terraform
+        formatting.terrafmt,
+    },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 })
-
-vim.cmd([[
-    augroup FormatAutogroup
-        autocmd!
-        autocmd! BufWritePre * lua vim.lsp.buf.formatting_sync()
-    augroup END
-]])
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,123 +1,94 @@
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-	print("Please, ensure install of `neovim/nvim-lspconfig` plugin")
-	return
+    print("Please, ensure install of `neovim/nvim-lspconfig` plugin")
+    return
 end
 
 local protocol = require("vim.lsp.protocol")
 
 protocol.CompletionItemKind = {
-	"", -- Text
-	"", -- Method
-	"", -- Function
-	"", -- Constructor
-	"", -- Field
-	"", -- Variable
-	"", -- Class
-	"ﰮ", -- Interface
-	"", -- Module
-	"", -- Property
-	"", -- Unit
-	"", -- Value
-	"", -- Enum
-	"", -- Keyword
-	"﬌", -- Snippet
-	"", -- Color
-	"", -- File
-	"", -- Reference
-	"", -- Folder
-	"", -- EnumMember
-	"", -- Constant
-	"", -- Struct
-	"", -- Event
-	"ﬦ", -- Operator
-	"", -- TypeParameter
+    "", -- Text
+    "", -- Method
+    "", -- Function
+    "", -- Constructor
+    "", -- Field
+    "", -- Variable
+    "", -- Class
+    "ﰮ", -- Interface
+    "", -- Module
+    "", -- Property
+    "", -- Unit
+    "", -- Value
+    "", -- Enum
+    "", -- Keyword
+    "﬌", -- Snippet
+    "", -- Color
+    "", -- File
+    "", -- Reference
+    "", -- Folder
+    "", -- EnumMember
+    "", -- Constant
+    "", -- Struct
+    "", -- Event
+    "ﬦ", -- Operator
+    "", -- TypeParameter
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "●" },
-	severity_sort = true,
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
 })
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = "", Warn = "", Hint = "ﴞ", Info = "" }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-	virtual_text = {
-		prefix = "●",
-	},
-	update_in_insert = true,
-	float = {
-		source = "always", -- Or "if_many"
-	},
+    virtual_text = {
+        prefix = "●",
+    },
+    update_in_insert = true,
+    float = {
+        source = "always", -- Or "if_many"
+    },
 })
 
 -- Setup
 --------------------------------------------------------------------------------
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-local enable_format_on_save = function(_, bufnr)
-	vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		group = augroup_format,
-		buffer = bufnr,
-		callback = function()
-			vim.lsp.buf.format({ bufnr = bufnr })
-		end,
-	})
-end
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-local formatters_to_be_disabled = { "tsserver", "sumneko_lua", "jsonls" }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "<space>k", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, bufopts)
+    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set("n", "gl", function()
+        vim.diagnostic.open_float({ border = "rounded" })
+    end, bufopts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    vim.keymap.set("n", "<space>f", function()
+        vim.lsp.buf.format({ async = true })
+    end, bufopts)
 
-local has_value = function(tab, val)
-	for _, value in ipairs(tab) do
-		if value == val then
-			return true
-		end
-	end
-
-	return false
-end
-
-local on_attach = function(client, bufnr)
-	if has_value(formatters_to_be_disabled, client.name) then
-		client.server_capabilities.documentFormattingProvider = false
-	end
-
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "<space>k", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, bufopts)
-	-- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "gl", function()
-		vim.diagnostic.open_float({ border = "rounded" })
-	end, bufopts)
-	vim.keymap.set("n", "<space>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>f", function()
-		vim.lsp.buf.format({ async = true })
-	end, bufopts)
-
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
 end
 
 -- Language Specific LSP
@@ -129,57 +100,53 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- JS and TS
 --------------------------------------------------------------------------------
 lspconfig.tsserver.setup({
-	on_attach = on_attach,
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-	cmd = { "typescript-language-server", "--stdio" },
-	capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" },
+    capabilities = capabilities,
 })
 
 -- Lua
 --------------------------------------------------------------------------------
 lspconfig.sumneko_lua.setup({
-	capabilities = capabilities,
-	on_attach = function(client, bufnr)
-		on_attach(client, bufnr)
-		enable_format_on_save(client, bufnr)
-	end,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true), -- Make the server aware of Neovim runtime files
-				checkThirdParty = false,
-			},
-			runtime = {
-				version = "LuaJIT",
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true), -- Make the server aware of Neovim runtime files
+                checkThirdParty = false,
+            },
+            runtime = {
+                version = "LuaJIT",
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
 
 -- CSS
 --------------------------------------------------------------------------------
 lspconfig.cssls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- Terraform
 --------------------------------------------------------------------------------
 lspconfig.terraformls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- JSON
 --------------------------------------------------------------------------------
 lspconfig.jsonls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	init_options = { provideFormatter = true },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
