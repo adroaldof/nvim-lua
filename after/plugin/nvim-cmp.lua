8e724252500efcee95e9aff87afa107889d4a0ca
Adroaldo de Andrade <adroaldof@gmail.com>
2023-01-27 19:37:19 -0300

    Remove tag and shift tab from completion

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -26,11 +26,6 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -44,35 +39,15 @@ cmp.setup({
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" }, -- lsp
         { name = "nvim_lsp_signature_help" },
         { name = "luasnip" }, -- snippets
         { name = "path" }, -- file system paths
-    }, {
         { name = "buffer" }, -- text within current buffer
     }),
     formatting = {
@@ -89,6 +64,7 @@ cmp.setup({
         end,
     },
     window = {
+        completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
     enabled = function()
